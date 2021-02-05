1645e10e43e8c2566c31e9db86381d2631212d41
Daniel Strano <stranoj@gmail.com>
2021-02-05 10:35:38 -0500

    Remove Common

--- src/Simulation/Common/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
-using System.Runtime.CompilerServices;
-
-// Allow the test assembly to use our internal methods
-[assembly: InternalsVisibleTo("Tests.Microsoft.Quantum.Simulators" + SigningConstants.PUBLIC_KEY)]
